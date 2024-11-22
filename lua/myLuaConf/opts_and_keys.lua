7a2acfcda21d69155cd175ce0cf9a17368ca610d
John Wilger <john@johnwilger.com>
2024-11-22 08:05:47 -0800

    Update indentation settings

--- lua/myLuaConf/opts_and_keys.lua
+++ lua/myLuaConf/opts_and_keys.lua
@@ -45,12 +45,12 @@ vim.o.mouse = 'a'
 -- Indent
 vim.o.smarttab = true
 vim.opt.cpoptions:append('I')
--- vim.o.expandtab = true
--- vim.o.smartindent = true
--- vim.o.autoindent = true
--- vim.o.tabstop = 4
--- vim.o.softtabstop = 4
--- vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 
 -- stops line wrapping from being confusing
 vim.o.breakindent = true
