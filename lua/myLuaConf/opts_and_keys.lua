d4f45a5f571b0a42dd0bd2c0c4130c30e2dbb027
John Wilger <john@johnwilger.com>
2024-11-23 16:23:43 -0800

    use <c-s> to save files

--- lua/myLuaConf/opts_and_keys.lua
+++ lua/myLuaConf/opts_and_keys.lua
@@ -135,6 +135,8 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+vim.keymap.set('n', '<c-s>', '<cmd>write<cr>', { noremap = true, silent = true })
+vim.keymap.set('i', '<c-s>', '<esc><cmd>write<cr>', { noremap = true, silent = true })
 
 -- kickstart.nvim starts you with this. 
 -- But it constantly clobbers your system clipboard whenever you delete anything.
