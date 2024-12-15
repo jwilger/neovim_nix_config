5c753adccf779de95f95e51f0de5ebb22a6f204d
John Wilger <john@johnwilger.com>
2024-12-15 13:31:24 -0800

    Fix nvim-tree keybindings and follow file
    When I set up the toggle, I inadvertently disabled all of the *other*
    nvim-tree keybindings. Fixed that. Also added the setting to have the
    selected file in nvim-tree follow the active file in the editing
    buffer.

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -22,8 +22,14 @@ if nixCats("general.extra") then
 	vim.g.loaded_netrw = 1
 	vim.g.loaded_netrwPlugin = 1
 	require("nvim-tree").setup({
+        update_focused_file = {
+            enable = true,
+            update_cwd = false
+        },
 		on_attach = function(bufnr)
 			local api = require("nvim-tree.api")
+
+            api.config.mappings.default_on_attach(bufnr)
 
 			-- Key mappings specific to the nvim-tree buffer
 			local opts = { buffer = bufnr, noremap = true, silent = true, desc = "Toggle Nvim Tree File Browser" }
