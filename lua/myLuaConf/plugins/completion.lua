b14e6c27a78716a71bc298c6074c3516b59d3d1a
John Wilger <john@johnwilger.com>
2024-12-17 19:59:58 -0800

    Fixed some keybindings for completions and snippets
    This makes it so that when editing a multi-placeholder snippet, you can
    hit TAB/S-TAB to move between placeholders, and it makes it so that
    hitting enter when the completion popup is present will not auto-select
    the first item in the list when you haven't selected anything; you'll
    get an actual newline in the buffer.

--- lua/myLuaConf/plugins/completion.lua
+++ lua/myLuaConf/plugins/completion.lua
@@ -130,19 +130,23 @@ return {
 					["<C-Space>"] = cmp.mapping.complete({}),
 					["<CR>"] = cmp.mapping.confirm({
 						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
+						select = false,
 					}),
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
 						else
 							fallback()
 						end
-					end, { "i", "s" }),
+					end, { "i", "s" }), -- Enable in insert and select modes
 
 					["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
 						else
 							fallback()
 						end
